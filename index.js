5bc74149804dc6fc1f770a3a909e5babdb13b5d4
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2021-11-15 16:20:30 -0300

    hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const config = require('./config/config');
+const app = express();
+
+const port = config.port;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
